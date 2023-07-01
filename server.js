e19e2a3d7ac8a714773d547244e1f61d96ea137d
VishmayaGithub <72074398+VishmayaGithub@users.noreply.github.com>
2023-07-01 14:32:41 +0530

    second commit

--- server.js
+++ server.js
@@ -3,19 +3,46 @@ const app = express();
 const server = require("http").Server(app);
 app.use(express.json())
 
-server.listen(process.env.PORT || 3030);
+var nodemailer = require('nodemailer');
 
-var nodemailer = require("nodemailer");
-const { request } = require("http");
 const transporter = nodemailer.createTransport({
-    port:587,
-    host:"smtp.gmail.com",
+    port: 465,
+    host: "smtp.gmail.com",
     auth:{
         user:"drsbalupriya@gmail.com",
-        pass:"nwbwckdfsacuvwmw"
+        pass:"thcowzrdnrjezwta"
+    },
+    secure: true,
+});
 
-},
-secure:true,
-
-
+app.post("/send-mail", (req, res) => {
+    const to = req.body.to;
+    const name = req.body.name;
+    const amount = req.body.amount;
+    const date = req.body.date;
+    const mailData = {
+        from: "drsbalupriya@gmail.com",
+        to: to,
+        subject: "Your payment is due!",
+        html: ` <p>
+                    Hello ${name},
+                </p>
+                <p>
+                    This is a reminder email that your payment of amount - ${amount} is due on date - ${date}
+                </p>
+                <p>
+                    Kindly make the payment before the due date to avoid any inconvenience.
+                </p>
+                <p>
+                    Thanks and Regards,
+                </p>`
+    };
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        res.status(200).send({ message: "Invitation sent!", message_id: info.messageId });
+    });
 })
+
+server.listen(process.env.PORT || 3030);
